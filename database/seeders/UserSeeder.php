3d49b38314a5c706c880dbbc85a5890954919ffe
madkilin <mad.kilin@gmail.com>
2025-10-05 19:36:59 +0700

    initial stable release (v1.0.0) - first version of waste complaint and education web app

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,26 +16,74 @@ class UserSeeder extends Seeder
         $data = [
             [
                 'name' => 'Administrator',
-                'nik' => '12345',
-                'phone' => '081111',
+                'nik' => '1234567890123451',
+                'phone' => '08123456781',
                 'address' => 'alamat 1',
                 'email' => 'admin@mail.com',
                 'role_id' => 1
             ],
             [
-                'name' => 'Petugas Lapangan',
-                'nik' => '12345',
-                'phone' => '081111',
+                'name' => 'Administrator 2',
+                'nik' => '1234567890123452',
+                'phone' => '08123456782',
                 'address' => 'alamat 2',
+                'email' => 'admin2@mail.com',
+                'role_id' => 1
+            ],
+            [
+                'name' => 'Petugas Lapangan',
+                'nik' => '1234567890123453',
+                'phone' => '08123456783',
+                'address' => 'alamat 3',
                 'email' => 'petugas@mail.com',
                 'role_id' => 2
             ],
             [
-                'name' => 'Masyarakat',
-                'nik' => '12345',
-                'phone' => '081111',
-                'address' => 'alamat 1',
+                'name' => 'Petugas Lapangan 2',
+                'nik' => '1234567890123454',
+                'phone' => '08123456784',
+                'address' => 'alamat 4',
+                'email' => 'petugas2@mail.com',
+                'role_id' => 2
+            ],
+            [
+                'name' => 'Masyarakat 1',
+                'nik' => '1234567890123455',
+                'phone' => '081234567811',
+                'address' => 'alamat a',
                 'email' => 'masyarakat@mail.com',
+                'role_id' => 3
+            ],
+            [
+                'name' => 'Masyarakat 2',
+                'nik' => '1234567890123456',
+                'phone' => '081234567812',
+                'address' => 'alamat b',
+                'email' => 'masyarakat2@mail.com',
+                'role_id' => 3
+            ],
+            [
+                'name' => 'Masyarakat 3',
+                'nik' => '1234567890123457',
+                'phone' => '081234567813',
+                'address' => 'alamat c',
+                'email' => 'masyarakat3@mail.com',
+                'role_id' => 3
+            ],
+            [
+                'name' => 'Masyarakat 4',
+                'nik' => '1234567890123458',
+                'phone' => '081234567814',
+                'address' => 'alamat d',
+                'email' => 'masyarakat4@mail.com',
+                'role_id' => 3
+            ],
+            [
+                'name' => 'Masyarakat 5',
+                'nik' => '1234567890123459',
+                'phone' => '081234567815',
+                'address' => 'alamat e',
+                'email' => 'masyarakat5@mail.com',
                 'role_id' => 3
             ],
         ];
@@ -43,7 +91,7 @@ class UserSeeder extends Seeder
         foreach ($data as $value) {
             User::updateOrCreate([
                 'email' => $value['email']
-            ],[
+            ], [
                 'password' => bcrypt('password'),
                 'name' => $value['name'],
                 'nik' => $value['nik'],
